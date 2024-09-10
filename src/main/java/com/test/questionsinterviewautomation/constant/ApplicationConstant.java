8883e531f8d2bae6e656355a63fd1f7972ced684
Sushant <usuanthja279@gmail.com>
2024-09-10 16:42:03 +0530

    Constant Additions

--- src/main/java/com/test/questionsinterviewautomation/constant/ApplicationConstant.java
+++ src/main/java/com/test/questionsinterviewautomation/constant/ApplicationConstant.java
@@ -5,8 +5,8 @@ import java.util.List;
 public class ApplicationConstant {
     private ApplicationConstant() {
     }
-
-
+    public static final String HISTORY_FILE_PATH = "E:/questions-interview-automation/src/main/resources/prompt-response-history.json";
+    public static final String LEVEL_FILE_PATH = "E:/questions-interview-automation/src/main/resources/level.json";
     public static final List<String> typeList = List.of("dsaProblems", "sqlProblems", "systemDesignProblems");
     public static final List<String> mailList = List.of("usuanhthja279@gmail.com", "naibgagan2@gmail.com", "47salman@gmail.com");
 }
